48450b284aca8fca175776613c0b7186b4e17f6a
ft-secret <120125662+ft-secret@users.noreply.github.com>
2022-12-08 20:43:01 +0300

    Update wow.sh

--- wow.sh
+++ wow.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
 rm -rf secret98
 
 git clone https://github.com/ft-secret/secret98.git
+
+rm -rf secret98/.git*
+
+rm -rf secret98/.DS_Store*
 
 rm -rf /Library/Caches/com.google.androidwebkit
 
